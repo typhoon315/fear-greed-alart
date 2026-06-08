264401b120d32ef59356c2f4b279dab0de4b6b18
typhoon315 <taehooni21@gmail.com>
2026-06-08 21:36:45 +0900

    Update Fear_Greed.py

--- Fear_Greed.py
+++ Fear_Greed.py
@@ -9,7 +9,7 @@ url = "https://production.dataviz.cnn.io/index/fearandgreed/graphdata"
 try:
     data = requests.get(url, timeout=20).json()
 
-    score = int(data["fear_and_greed"]["score"])
+    score = int(data["Fear_and_Greed"]["score"])
 
     if score <= 100:
         message = (
